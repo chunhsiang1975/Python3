93b6af09a7cf291266905d883186a2134f894772
Chun-Hsiang Chao <chunhsiang2009@gmail.com>
2025-06-17 17:05:22 +0800

    Add files via upload

--- create_months_datas.py
+++ create_months_datas.py
@@ -1,5 +1,5 @@
 # Writeten by Chun-Hsiang Chao
-# Date:20250613
+# Date:20250617
 import datetime
 import openpyxl
 from openpyxl.styles import Font, Color, Alignment, Border, Side, PatternFill, NamedStyle, GradientFill
@@ -7,6 +7,10 @@ from openpyxl.utils import FORMULAE
 from openpyxl.formatting.rule import CellIsRule, ColorScaleRule
 from openpyxl.chart import BarChart, Reference
 from openpyxl.worksheet.formula import ArrayFormula
+from openpyxl.formula.translate import Translator
+from openpyxl.cell.cell import Cell
+#from xlcalculator import Evaluator
+#pip install xlcalculator --break-system-packages
 
 table_name=["202507","202507預算","202508","202508預算"]
 
@@ -21,15 +25,21 @@ sheet.page_setup.fitToWidth=1
 source_sheet=workbook["yearMonth"]
 workbook.copy_worksheet(source_sheet)
 workbook["yearMonth Copy"].title=table_name[0]
+sheet1=workbook[table_name[0]]
 
 source_sheet=workbook["yearMonth預算"]
 workbook.copy_worksheet(source_sheet)
 workbook["yearMonth預算 Copy"].title=table_name[1]
+
+
 sheet=workbook[table_name[1]]
 sheet['B2']=table_name[1][4:6]
 
 string_to_replace = "yearMonth"
-replacement_string ="$'"+table_name[0]+"'"
+replacement_string ="$"+table_name[0]+""
+
+#change to this format ='202507'.$D$98 not ='$202507'.$D$98
+
 
 amountOfRows = sheet.max_row
 amountOfColumns = sheet.max_column
@@ -41,11 +51,13 @@ for r in range(1,sheet.max_row+1):
         if s != None and string_to_replace in str(s):
             sheet.cell(r,c).value = s.replace(string_to_replace,replacement_string)
             s_list=list(sheet.cell(r,c).value)
-            s_list[10]="."
+            s_list[8]="."
             new_s=''.join(s_list)
             print(new_s)
-            #sheet.cell(r,c).value=new_s
-            sheet.cell(r,c).value='=SUM(D4:D100)'
+            sheet.cell(r,c).value=new_s
+						
+            #sheet.cell(r,c).value='='+'SUM($202507.D4:D100)'
+            sheet.cell(r,c).data_type='f'
 
             print("row {} col {} : {}".format(r,c,s))
             i += 1
