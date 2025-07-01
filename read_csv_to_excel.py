d3b9a1b3275d81461ee5cb07219ecfbd63af417e
Chun-Hsiang Chao <chunhsiang2009@gmail.com>
2025-07-01 16:50:51 +0800

    Add files via upload

--- read_csv_to_excel.py
+++ read_csv_to_excel.py
@@ -1,5 +1,5 @@
 # Writeten by Chun-Hsiang Chao
-# Date:20250626
+# Date:20250701
 import csv
 import openpyxl
 import os
@@ -16,9 +16,7 @@ def convertDate(date):
 	return realdate
 
 
-
 file_path = 'STOCK_DAY_1101_202506_utf-8.csv'
-
 
 
 wb = Workbook()
@@ -31,13 +29,28 @@ with open(file_path, 'r', newline='\n', encoding='utf-8') as f:
 		sheet.append(row)
 
 
-for x in range(3,22):
+records_number=21
+
+for x in range(3,records_number+3):
   for y in range(4,9):
     sheet.cell(row=x,column=y,value=float(sheet.cell(row=x,column=y).value))
 
-
-for x in range(3,22):
+for x in range(3,records_number+3):
 	sheet.cell(row=x,column=1,value=(convertDate(sheet.cell(row=x,column=1).value)))
+
+int_columns=[2,3,9]
+for x in range(3,records_number+3):
+  for y in int_columns:
+    s_list=list(sheet.cell(row=x,column=y).value)
+    new_s_list=[]
+    for z in range(0,len(s_list)):
+      if (s_list[z]!=","):
+          new_s_list.append(s_list[z])
+    new_s=''.join(new_s_list)
+    sheet.cell(row=x,column=y,value=int(new_s))
+
+
+
 
 i=get_column_letter(1)
 sheet.column_dimensions[i].width=10
@@ -61,29 +74,53 @@ sheet.column_dimensions[i].width=10
 chart=LineChart()
 #Method 1
 #data = Reference(sheet,min_row=2,max_row=21,min_col=4,max_col=7)
-#chart.add_data(data_1, titles_from_data=True)
+#chart.add_data(data, titles_from_data=True)
 
 #Method 2
 color_name=["F0F000","FF0000","00FF00","0000FF"]
 for i in range(0,4):
-	data = Reference(sheet,min_row=3,max_row=21,min_col=4+i,max_col=4+i)
+	data = Reference(sheet,min_row=3,max_row=records_number+2,min_col=4+i,max_col=4+i)
 	series=Series(data,title=sheet.cell(row=2,column=4+i).value)
 	series.graphicalProperties.line.solidFill = color_name[i]
 	chart.append(series)
 
 
 
-
-
-
-chart.y_axis.scaling.min = 24  # Set the minimum value for the y-axis
-chart.y_axis.scaling.max = 29 # Set the maximum value for the y-axis
-
-
+#chart.y_axis.scaling.min = 24  # Set the minimum value for the y-axis
+#chart.y_axis.scaling.max = 29 # Set the maximum value for the y-axis
 
 sheet.add_chart(chart, "J2")
 
+chart=BarChart()
+datas_columns=[2]
+for i in datas_columns:
+	data = Reference(sheet,min_row=3,max_row=records_number+2,min_col=i,max_col=i)
+	series=Series(data,title=sheet.cell(row=2,column=i).value)
+	series.graphicalProperties.line.solidFill = color_name[0]
+	chart.append(series)
 
+sheet.add_chart(chart, "J20")
+
+
+chart=BarChart()
+datas_columns=[3]
+for i in datas_columns:
+	data = Reference(sheet,min_row=3,max_row=records_number+2,min_col=i,max_col=i)
+	series=Series(data,title=sheet.cell(row=2,column=i).value)
+	series.graphicalProperties.line.solidFill = color_name[1]
+	chart.append(series)
+
+sheet.add_chart(chart, "S2")
+
+chart=BarChart()
+datas_columns=[9]
+for i in datas_columns:
+	data = Reference(sheet,min_row=3,max_row=records_number+2,min_col=i,max_col=i)
+	series=Series(data,title=sheet.cell(row=2,column=i).value)
+	series.graphicalProperties.line.solidFill = color_name[2]
+	chart.append(series)
+
+sheet.add_chart(chart, "S20")
 
 wb.save('output_excel_file.xlsx')
 wb.close()
