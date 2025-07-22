2c447a35a30929ab93d65a85813e14b4195186c9
Chun-Hsiang Chao <chunhsiang2009@gmail.com>
2025-07-22 15:41:44 +0800

    Add files via upload

--- pandasPlot1.py
+++ pandasPlot1.py
@@ -1,5 +1,5 @@
 # Writeten by Chun-Hsiang Chao
-# Date:20250717
+# Date:20250722
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
@@ -10,5 +10,6 @@ indexs = ["賣場A", "賣場B", "賣場C", "賣場D"]
 columns = ["雞肉", "水果", "蔬菜", "牛奶", "麵包"]
 df = pd.DataFrame(datas, columns=columns,  index=indexs)
 df.plot(kind='bar', title='賣場銷售額', fontsize=12)
+plt.xticks(rotation=45)
 plt.savefig("dataframe.png")
 plt.show()
