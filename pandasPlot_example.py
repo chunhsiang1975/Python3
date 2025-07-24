50c9b8855d7ba55f36c813ae08f1425f5f66fd31
Chun-Hsiang Chao <chunhsiang2009@gmail.com>
2025-07-24 17:04:55 +0800

    Add files via upload

--- pandasPlot_example.py
+++ pandasPlot_example.py
@@ -24,40 +24,54 @@ records_number=rows-7
 
 row_number=2
 column_name = [cell.value for cell in sheet[row_number]] 
-#print(column_name)
+#print(list(column_name)[1])
 
-data=[]
+
+row_number=2
+df=pd.DataFrame(list([cell.value for cell in sheet[row_number]]))
+#df.columns=list(column_name)
+print(len(df.columns))
 for i in range(3,records_number+3):
   row_data = [cell.value for cell in sheet[i]] 
-  print(row_data) 
-  data=data+list(row_data)
+  #print(row_data)
+  new_df=pd.DataFrame(row_data)
+  df = pd.concat([df, new_df], ignore_index=True)
+
+print(df.loc[[0,11]])
+print(df.columns.tolist())
 
 
 #df=pd.DataFrame(data,columns=column_name)
-df=pd.DataFrame(data)
-
-#df = pd.read_csv('BWIBBU_d_ALL_20250701_utf-8.csv')
+#df = pd.read_csv(file_path,sep=',')
 #df = pd.read_csv('data.csv')
 
-pd.options.display.max_rows=9999
+full_df = pd.read_csv(file_path,sep=',')
+new_row_series=full_df.iloc[1]
+print(new_row_series)
+
+
+
+#pd.options.display.max_rows=9999
 #print(df.to_string()) 
 #print(df.loc[[0,11]])
 #print(df.head())
 #print(df.tail())
 #print(df.info())
-print(df)
+#print(df)
+#print(len(df.columns))
+#print(df.columns.tolist())
 
-a=[1,7,2]
-var=pd.Series(a,index=["x","y","z"])
+#a=[1,7,2]
+#var=pd.Series(a,index=["x","y","z"])
 #print(var)
 
-calories = {"day1": 420, "day2": 380, "day3": 390}
-var = pd.Series(calories)
+#calories = {"day1": 420, "day2": 380, "day3": 390}
+#var = pd.Series(calories)
 #print(var)
 
-data = {
-  "calories": [420, 380, 390],
-  "duration": [50, 40, 45]
-}
-var = pd.DataFrame(data)
+#data = {
+#  "calories": [420, 380, 390],
+#  "duration": [50, 40, 45]
+#}
+#var = pd.DataFrame(data)
 #print(var)
